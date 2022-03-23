e6a782b52b92acca9f3c82736e52a5b990acdaa8
Jonathas Nascimento <nasc.jonathas@gmail.com>
2022-03-23 09:33:14 -0300

    add category registration component

---
+++ src/app/categories/category-registration/category-registration.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-category-registration',
+  templateUrl: './category-registration.component.html',
+  styleUrls: ['./category-registration.component.css']
+})
+export class CategoryRegistrationComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}

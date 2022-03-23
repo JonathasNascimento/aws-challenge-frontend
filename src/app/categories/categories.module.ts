4cbdbdb268e81877133ae54934e51d13055deae3
Jonathas Nascimento <nasc.jonathas@gmail.com>
2022-03-23 09:34:20 -0300

    add categories components to module

--- src/app/categories/categories.module.ts
+++ src/app/categories/categories.module.ts
@@ -1,17 +1,14 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
 
+import { MaterialModule } from '../material.module';
 import { CategoriesRoutingModule } from './categories-routing.module';
 import { CategoriesComponent } from './categories.component';
-
+import { CategoryListComponent } from './category-list/category-list.component';
+import { CategoryRegistrationComponent } from './category-registration/category-registration.component';
 
 @NgModule({
-  declarations: [
-    CategoriesComponent
-  ],
-  imports: [
-    CommonModule,
-    CategoriesRoutingModule
-  ]
+  declarations: [CategoriesComponent, CategoryListComponent, CategoryRegistrationComponent],
+  imports: [CommonModule, CategoriesRoutingModule, MaterialModule],
 })
-export class CategoriesModule { }
+export class CategoriesModule {}

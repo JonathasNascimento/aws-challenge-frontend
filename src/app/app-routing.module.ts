2fd9b39ebb52500ff2c6205e6d64fbbe6d52eec7
Jonathas Nascimento <nasc.jonathas@gmail.com>
2022-03-22 22:41:20 -0300

    added categories module and components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [{ path: 'devices', loadChildren: () => import('./devices/devices.module').then(m => m.DevicesModule) }];
+const routes: Routes = [{ path: 'devices', loadChildren: () => import('./devices/devices.module').then(m => m.DevicesModule) }, { path: 'categories', loadChildren: () => import('./categories/categories.module').then(m => m.CategoriesModule) }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],

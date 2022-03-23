972bed156c8865d2caba122a04b30589b1e474cb
Jonathas Nascimento <nasc.jonathas@gmail.com>
2022-03-23 09:14:39 -0300

    add mock for device list

--- src/app/devices/device-list/device-list.component.ts
+++ src/app/devices/device-list/device-list.component.ts
@@ -3,13 +3,26 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-device-list',
   templateUrl: './device-list.component.html',
-  styleUrls: ['./device-list.component.css']
+  styleUrls: ['./device-list.component.css'],
 })
 export class DeviceListComponent implements OnInit {
+  displayedColumns: string[] = ['category', 'color', 'partNumber', 'action']
+  dataSource = [
+    { category: 'cat-1', color: 'white', partNumber: '12345' },
+    { category: 'cat-1', color: 'white', partNumber: '12345' },
+    { category: 'cat-1', color: 'white', partNumber: '12345' },
+    { category: 'cat-1', color: 'white', partNumber: '12345' },
+    { category: 'cat-1', color: 'white', partNumber: '12345' },
+    { category: 'cat-1', color: 'white', partNumber: '12345' },
+    { category: 'cat-1', color: 'white', partNumber: '12345' },
+    { category: 'cat-1', color: 'white', partNumber: '12345' },
+    { category: 'cat-1', color: 'white', partNumber: '12345' },
+    { category: 'cat-1', color: 'white', partNumber: '12345' },
+    { category: 'cat-1', color: 'white', partNumber: '12345' },
+    { category: 'cat-1', color: 'white', partNumber: '12345' },
+  ]
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }

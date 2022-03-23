c4125471231eb12d58591b43df8ae0f5bc5680c5
Jonathas Nascimento <nasc.jonathas@gmail.com>
2022-03-23 07:19:04 -0300

    created device registration form

--- src/app/devices/device-registration/device-registration.component.ts
+++ src/app/devices/device-registration/device-registration.component.ts
@@ -3,13 +3,16 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-device-registration',
   templateUrl: './device-registration.component.html',
-  styleUrls: ['./device-registration.component.css']
+  styleUrls: ['./device-registration.component.css'],
 })
 export class DeviceRegistrationComponent implements OnInit {
+  categories = [
+    { value: 'cat-0', viewValue: 'cat-0' },
+    { value: 'cat-1', viewValue: 'cat-1' },
+    { value: 'cat-2', viewValue: 'cat-2' },
+  ]
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -1,11 +1,22 @@
 import { NgModule } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
+import { MatDividerModule } from '@angular/material/divider';
 import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatSelectModule } from '@angular/material/select';
 import { MatToolbarModule } from '@angular/material/toolbar';
 
 @NgModule({
-  exports: [MatToolbarModule, MatIconModule, MatButtonModule, MatCardModule],
+  exports: [
+    MatToolbarModule,
+    MatIconModule,
+    MatButtonModule,
+    MatCardModule,
+    MatDividerModule,
+    MatInputModule,
+    MatSelectModule,
+  ],
   providers: [],
 })
 export class MaterialModule {}

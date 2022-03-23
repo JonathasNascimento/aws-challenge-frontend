1bfa255ce937bbbf8a82d17d95f7268f4e1752ec
Jonathas Nascimento <nasc.jonathas@gmail.com>
2022-03-22 23:59:34 -0300

    add component to device registration

---
+++ src/app/devices/device-registration/device-registration.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-device-registration',
+  templateUrl: './device-registration.component.html',
+  styleUrls: ['./device-registration.component.css']
+})
+export class DeviceRegistrationComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}

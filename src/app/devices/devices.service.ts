c953c8c79b0ba8f0a14cd34355f1916b38c2e4cf
Jonathas Nascimento <nasc.jonathas@gmail.com>
2022-03-24 00:38:08 -0300

    create methods interface in services

--- src/app/devices/devices.service.ts
+++ src/app/devices/devices.service.ts
@@ -1,9 +1,19 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+import { HttpResponse } from '../commons/http-response';
+import { Device } from './device.model';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class DevicesService {
+  constructor(private http: HttpClient) {}
 
-  constructor() { }
+  findAllDevices(): Observable<HttpResponse<Device[]>> {}
+
+  createNewDevices(device: Device): Observable<HttpResponse<Device>> {}
+
+  deleteDevice(id: number): Observable<HttpResponse<Device>> {}
 }

2e67191df50ae98b56779bc86fe9211236cd4587
Jonathas Nascimento <nasc.jonathas@gmail.com>
2022-03-24 00:43:57 -0300

    implement devices services methods

--- src/app/devices/devices.service.ts
+++ src/app/devices/devices.service.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { getApi } from 'src/environments/environment';
 
 import { HttpResponse } from '../commons/http-response';
 import { Device } from './device.model';
@@ -9,11 +10,19 @@ import { Device } from './device.model';
   providedIn: 'root',
 })
 export class DevicesService {
+  private devicesUrl = getApi('devices')
+
   constructor(private http: HttpClient) {}
 
-  findAllDevices(): Observable<HttpResponse<Device[]>> {}
+  findAllDevices(): Observable<HttpResponse<Device[]>> {
+    return this.http.get<HttpResponse<Device[]>>(this.devicesUrl)
+  }
 
-  createNewDevices(device: Device): Observable<HttpResponse<Device>> {}
+  createNewDevices(device: Device): Observable<HttpResponse<Device>> {
+    return this.http.post<HttpResponse<Device>>(this.devicesUrl, device)
+  }
 
-  deleteDevice(id: number): Observable<HttpResponse<Device>> {}
+  deleteDevice(id: number): Observable<HttpResponse<Device>> {
+    return this.http.delete<HttpResponse<Device>>(`${this.devicesUrl}/${id}`)
+  }
 }

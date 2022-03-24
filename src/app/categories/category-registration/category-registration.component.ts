56983084c99f6f1f5c1d58d6465837cdfcf83757
Jonathas Nascimento <nasc.jonathas@gmail.com>
2022-03-23 22:50:10 -0300

    integrate add category with backend

--- src/app/categories/category-registration/category-registration.component.ts
+++ src/app/categories/category-registration/category-registration.component.ts
@@ -1,15 +1,41 @@
 import { Component, OnInit } from '@angular/core';
+import { MatSnackBar } from '@angular/material/snack-bar';
+
+import { CategoriesService } from '../categories.service';
+import { Category } from '../category.model';
 
 @Component({
   selector: 'app-category-registration',
   templateUrl: './category-registration.component.html',
-  styleUrls: ['./category-registration.component.css']
+  styleUrls: ['./category-registration.component.css'],
 })
 export class CategoryRegistrationComponent implements OnInit {
+  category: Category = { name: '' }
+  isLoading: boolean = false
 
-  constructor() { }
+  constructor(
+    private categoriesService: CategoriesService,
+    private snackBar: MatSnackBar,
+  ) {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  createNewCategory() {
+    this.isLoading = true
+    this.categoriesService
+      .createCategory(this.category)
+      .subscribe((response) => {
+        this.isLoading = false
+
+        this.showSnackBar(`Category '${this.category.name}' added successfully`)
+        this.category.name = ''
+      })
   }
 
+  private showSnackBar(message: string) {
+    this.snackBar.open(message, 'Done', {
+      horizontalPosition: 'start',
+      duration: 3000,
+    })
+  }
 }

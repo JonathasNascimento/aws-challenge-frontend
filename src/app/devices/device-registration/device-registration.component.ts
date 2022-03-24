3e132e34b9b5d4a8b885bbeae435fe8cb7322e50
Jonathas Nascimento <nasc.jonathas@gmail.com>
2022-03-24 01:43:13 -0300

    integrate device with backend

--- src/app/devices/device-registration/device-registration.component.ts
+++ src/app/devices/device-registration/device-registration.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Category } from 'src/app/categories/category.model';
+
+import { Device } from '../device.model';
+import { DevicesService } from '../devices.service';
 
 @Component({
   selector: 'app-device-registration',
@@ -6,13 +11,40 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./device-registration.component.css'],
 })
 export class DeviceRegistrationComponent implements OnInit {
-  categories = [
-    { value: 'cat-0', viewValue: 'cat-0' },
-    { value: 'cat-1', viewValue: 'cat-1' },
-    { value: 'cat-2', viewValue: 'cat-2' },
-  ]
+  form: FormGroup
 
-  constructor() {}
+  constructor(
+    private devicesService: DevicesService,
+    private formBuilder: FormBuilder,
+  ) {
+    this.form = this.formBuilder.group({
+      color: [null, Validators.required],
+      partNumber: [null, [Validators.required]],
+      categoryId: [null, [Validators.required]],
+    })
+  }
 
   ngOnInit(): void {}
+
+  onCategorySelected(event: Category) {
+    const control = this.form.get('categoryId')
+    control?.setValue(event.id)
+  }
+
+  createDevice() {
+    const device: Device = {
+      categoryId: +this.getFieldValue('categoryId'),
+      color: this.getFieldValue('color'),
+      partNumber: +this.getFieldValue('partNumber'),
+    }
+    console.log(device)
+    this.devicesService.createNewDevices(device).subscribe((response) => {
+      console.log(response)
+    })
+  }
+
+  private getFieldValue(key: string): string {
+    const control = this.form.get(key)
+    return control?.value
+  }
 }
